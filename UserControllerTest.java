fcdb1417d0b41c69abd12d788450af322cd90779
Adam Greenan <87552818+plutowootoe@users.noreply.github.com>
2024-10-18 17:25:59 +0100

    Update UserControllerTest.java

--- UserControllerTest.java
+++ UserControllerTest.java
@@ -12,13 +12,12 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
 
